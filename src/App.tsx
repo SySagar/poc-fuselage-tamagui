570efa0fc88a4d4ad34e079c3312a0a424b337d9
Sagar <sysagar07@gmail.com>
2024-03-30 21:00:43 +0530

    feat: basic select using tamagui

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import '@tamagui/core/reset.css'
+import { TamaguiProvider} from '@tamagui/core';
+import {SelectDemo} from './lib/Select'
+import './App.css'
+
+import config from './tamagui.config'
+
+// type Conf = typeof tamaguiConfig
+// declare module '@tamagui/core' {
+//   interface TamaguiCustomConfig extends Conf {}
+// }
+
+function App() {
+  
+  return (
+    <TamaguiProvider config={config}>
+    <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    
+    }}
+    >
+      <SelectDemo />
+    </div>
+    </TamaguiProvider>
+  )
+}
+
+export default App
